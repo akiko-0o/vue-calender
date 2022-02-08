af151b03007e84c156c33c511bdecbecef63e145
秋田亜希子 <a_akita@waku-2.com>
2022-02-08 19:08:16 +0900

    ディレクトリ作成

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <Calendar />
+  </div>
+</template>
+<script>
+import Calendar from './components/pages/Calendar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Calendar,
+  },
+};
+</script>
---
+++ src/components/pages/Calendar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="calendarWrapper">
+    <div class="calendarHeader"></div>
+    <div class="calendar"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
