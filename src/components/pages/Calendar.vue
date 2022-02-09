4703eb1c92f3d7f34eca5ba6dd2602f42fd8bec0
秋田亜希子 <a_akita@waku-2.com>
2022-02-09 18:44:30 +0900

    2/9作業分

--- src/components/pages/Calendar.vue
+++ src/components/pages/Calendar.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="calendarWrapper">
-    <div class="calendarHeader"></div>
+    <div class="calendarHeader">
+      <button>今日</button>
+      <div class="calendarHeader__pagination">
+        <button>&lt;</button>
+        <button>&gt;</button>
+      </div>
+      <p>年月</p>
+    </div>
     <div class="calendar"></div>
   </div>
 </template>
---
+++ src/components/parts/Modal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="modalWrapper">
+    <div>
+      <i>✖️</i>
+    </div>
+    <div class="modal">
+      <p>日付</p>
+      <input type="text" />
+      <button>保存</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
